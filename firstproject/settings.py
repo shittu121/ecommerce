b006d84b56505d5df7ee0cb9354bc27d4abb7dbe
shittu121 <shittufaruq6@gmail.com>
2024-10-27 08:29:49 +0000

    update

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-n*8#w=8f2zu6$h^#rci*+%8ljk!6hpx@t1l3q%vye58gnl49vj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -130,4 +131,6 @@ MEDIA_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
