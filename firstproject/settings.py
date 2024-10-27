bedf8da9deda8e860a3ac52186d67edfd189deb1
shittu121 <shittufaruq6@gmail.com>
2024-10-27 06:43:41 +0000

    Change to Postgressql

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n*8#w=8f2zu6$h^#rci*+%8ljk!6hpx@t1l3q%vye58gnl49vj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'firstproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',  # The path to the SQLite database file
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'jJvfRvOYgnRxxdxKxJbFlTpxkEiRSCxW',
+        'HOST': 'autorack.proxy.rlwy.net',
+        'PORT': '17625',
     }
 }
 
