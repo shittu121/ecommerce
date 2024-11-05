2f845a192af30cffd211094eb369862bf410b9db
shittu121 <shittufaruq6@gmail.com>
2024-11-05 18:35:27 +0000

    Fixed Images Issue

--- firstapp/models.py
+++ firstapp/models.py
@@ -13,7 +13,7 @@ def get_file_path(request, filename):
 class Category(models.Model):
     slug = models.CharField(max_length=150, null=False, blank=False)
     name = models.CharField(max_length=150, null=False, blank=False)
-    image = models.ImageField(upload_to=get_file_path, null=True, blank=True)
+    image = models.URLField(max_length=500, blank=True)
     description = models.TextField(max_length=500, null=False, blank=False)
     status = models.BooleanField(default=False, help_text="0=default, 1=Hidden")
     trending = models.BooleanField(default=False, help_text="0=default, 1=Trending")
@@ -29,7 +29,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     slug = models.CharField(max_length=150, null=False, blank=False)
     name = models.CharField(max_length=150, null=False, blank=False)
-    product_image = models.ImageField(upload_to=get_file_path, null=True, blank=True)
+    product_image = models.URLField(max_length=500, blank=True)
     small_description = models.TextField(max_length=250, null=False, blank=False)
     quantity= models.IntegerField(null=False, blank=False)
     description = models.CharField(max_length=500, null=False, blank=False)
